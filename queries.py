db087ca496498b7780957c69da529b0c692f0cb0
Selena Tuyen <sltuyen@ucdavis.edu>
2016-12-03 01:21:57 -0800

    FIX 3g

--- queries.py
+++ queries.py
@@ -30,36 +30,36 @@ for i in range(1,21):
 # 3b
 print '3b:'
 # non-weighted gpa
-# for i in range(1,21):
-#     cur.execute("""
-#         SELECT AVG(x.gpa) 
-#         FROM (
-#             SELECT CASE
-#                 WHEN GRADE = 'A+' OR GRADE = 'A' THEN 4.0
-#                 WHEN GRADE = 'A-' THEN 3.7
-#                 WHEN GRADE = 'B+' THEN 3.33
-#                 WHEN GRADE = 'B' THEN 3
-#                 WHEN GRADE = 'B-' THEN 2.7
-#                 WHEN GRADE = 'C+' THEN 2.3
-#                 WHEN GRADE = 'C' THEN 2
-#                 WHEN GRADE = 'C-' THEN 1.7
-#                 WHEN GRADE = 'D+' THEN 1.3
-#                 WHEN GRADE = 'D' THEN 1
-#                 WHEN GRADE = 'D-' THEN .7
-#                 WHEN GRADE = 'F' THEN 0
-#             END AS gpa
-#             FROM student, 
-#                 (SELECT id FROM (
-#                     SELECT TERM, SUM(student.UNITS) AS total, student.sid AS id 
-#                     FROM course, student 
-#                     WHERE student.scid = course.cid 
-#                     GROUP BY student.sid, TERM) AS comb 
-#                 WHERE comb.total = %s) AS tot
-#             WHERE student.sid = tot.id
-#         ) AS x
-#         """ % (i))
-#     result = cur.fetchall()
-#     print i,result
+for i in range(1,21):
+    cur.execute("""
+        SELECT AVG(x.gpa) 
+        FROM (
+            SELECT CASE
+                WHEN GRADE = 'A+' OR GRADE = 'A' THEN 4.0
+                WHEN GRADE = 'A-' THEN 3.7
+                WHEN GRADE = 'B+' THEN 3.33
+                WHEN GRADE = 'B' THEN 3
+                WHEN GRADE = 'B-' THEN 2.7
+                WHEN GRADE = 'C+' THEN 2.3
+                WHEN GRADE = 'C' THEN 2
+                WHEN GRADE = 'C-' THEN 1.7
+                WHEN GRADE = 'D+' THEN 1.3
+                WHEN GRADE = 'D' THEN 1
+                WHEN GRADE = 'D-' THEN .7
+                WHEN GRADE = 'F' THEN 0
+            END AS gpa
+            FROM student, 
+                (SELECT id FROM (
+                    SELECT TERM, SUM(student.UNITS) AS total, student.sid AS id 
+                    FROM course, student 
+                    WHERE student.scid = course.cid 
+                    GROUP BY student.sid, TERM) AS comb 
+                WHERE comb.total = %s) AS tot
+            WHERE student.sid = tot.id
+        ) AS x
+        """ % (i))
+    result = cur.fetchall()
+    print i,result
 
 # weighted gpa
 for i in range(1,21):
@@ -551,8 +551,23 @@ cur.execute("""
 majorCount= cur.fetchall()
 
 cur.execute("""
-    SELECT COUNT(DISTINCT SID)
-    FROM STUDENT;
+    SELECT SUM(st.totTrans)
+    FROM(
+        SELECT COUNT(DISTINCT theStudent.student1) AS totTrans
+        FROM
+            (SELECT DISTINCT sameSIDs.student1, sameSIDs.studentMaj
+            FROM
+                (SELECT s1.SID AS student1, s1.major AS studentMaj
+                 FROM student s1, student s2
+                 WHERE s1.SID = s2.SID AND s1.major NOT LIKE 'ABC%'
+                )AS sameSIDs) AS theStudent, roster r1, roster r2, student laterStudent
+        WHERE
+            theStudent.student1 = r1.RSID AND
+            theStudent.student1 = r2.RSID AND
+            theStudent.student1 = laterStudent.SID AND
+            laterStudent.major LIKE 'ABC%' AND
+            r1.RTERM < r2.RTERM
+        GROUP BY theStudent.studentMaj) AS st;
     """)
 
 total = cur.fetchall()
@@ -560,7 +575,7 @@ count = [x[0] for x in majorCount]
 major = [x[1] for x in majorCount]
 ans2 = [x[0] for x in total]
 for i in  range (0,5):
-    print float(count[i])/float(ans2[0]) * 100, major[i]
+    print float(count[i])/float(ans2[0]) * 100, '%',  major[i]
 
 cur.close()
 conn.close()
