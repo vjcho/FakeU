16c7d16e73201d6e755d0a94cfb731effc624764
Selena Tuyen <sltuyen@ucdavis.edu>
2016-12-03 13:25:53 -0800

    change term to int

--- openrm.py
+++ openrm.py
@@ -12,9 +12,9 @@ cur = conn.cursor()
 # Write a program that will find an open room for course expansion. The program must prompt for term, CID, and number students to add. The room(s) returned should be ordered from best to worst fit with up to 5 results. Assume that each room capacity is the maximum number of students listed for any particular meeting in the data files (don’t forget that
 # lectures may be split across multiple CIDs).
 
-term = input('Enter term: ')
-CID = input('Enter CID: ')
-numStu = input('Enter number of students: ')
+term = raw_input('Enter term: ')
+CID = raw_input('Enter CID: ')
+numStu = raw_input('Enter number of students: ')
 print term, CID, numStu
 
 cur.close()
